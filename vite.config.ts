736ec53a841d97f1ed4c642d369cc5f90b0f8cdd
NKvara <100515286+NKvara@users.noreply.github.com>
2025-04-08 20:57:24 +0400

    fix: did i really just miss a line

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsConfigPaths()],
-  base: "/portfolio"
+  base: "/portfolio/"
 })
